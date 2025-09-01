ecef20ac7e16e075cb0a2261e058e2972a21747d
sillyhatsonly <010635@list.ru>
2025-09-01 06:32:39 +0300

    add basic pytest setup

---
+++ tests/test_math_utils.py
@@ -0,0 +1,21 @@
+import pytest
+from math_utils import add
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (1, 1, 2),
+        (-1, 1, 0),
+        (100, -7, 93)
+    ],
+
+)
+
+def test_add_param(a, b, expected):
+    assert add(a, b) == expected
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_with_zero():
+    assert add(0, 7) == 7
